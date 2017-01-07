1acd69f31ef7f443e2c60e66fed913cdba3f1ffd
barneyJomo <brnbsjm@gmail.com>
2017-01-07 13:45:57 +0300

    categories fetch and listing done

--- src/Components/Categories/CategoryPage.js
+++ src/Components/Categories/CategoryPage.js
@@ -1,8 +1,13 @@
+/*eslint-disable react/no-did-mount-set-state */
+
 import React, {PropTypes} from 'react';
 import {connect} from 'react-redux';
+import {browserHistory} from 'react-router';
 import {loadCategories} from './CategoryActions';
 import toastr from 'toastr';
-import {Progress} from 'semantic-ui-react';
+import {Progress, Segment, Form, Button, Input, Header, Divider} from 'semantic-ui-react';
+import * as _ from 'lodash';
+import CategoryList from './CategoryList';
 
 class CategoryPage extends React.Component {
 
@@ -10,7 +15,7 @@ class CategoryPage extends React.Component {
     super(props, context);
 
     this.state = {
-      categories: {}, fetchingCategories: true, percent: 0
+      categories: [], fetchingCategories: true, percent: 50, addCategory: false
     };
   }
 
@@ -18,62 +23,68 @@ class CategoryPage extends React.Component {
     const categoryData = loadCategories();
     categoryData
       .then( (data) => {
-        console.log(data);
-        this.setState({fetchingCategories: false, categories: data});
+        this.setState({fetchingCategories: false, categories: data.categories, percent: 100});
       })
       .catch( (error) => {
         toastr.error(error);
       });
   }
 
-  componentDidMount() {
-    this.setState({percent: 100});
+  addCategory(){
+    browserHistory.push('/dashboard/category');
   }
+
 
   render () {
+    return (
+      <Segment.Group>
+        <Segment padded size="large"
+          color="blue"
+          className="text container">
+          <Form loading={this.state.addCategory}>
+            <Header size="medium">ADD A NEW CATEGORY</Header>
+            <Form.Field inline>
+              <label>Category Name</label>
+              <Input fluid
+                name="categoryName"
+                placeholder="Category Name" />
+            </Form.Field>
 
-    console.log("in Render")
-    console.log(this.state.fetchingCategories)
+            <Button primary color="blue" type="button"
+              name="submitCategory"
+              onClick={this.addCategory}
+              disabled={this.state.loggingIn}
+              content={this.state.loggingIn ? 'Processing...': 'Submit'} />
+          </Form>
+        </Segment>
 
-    if(this.state.fetchingCategories === true) {
-
-        return (
-          <div>
-            <Progress percent={this.state.percent}
-              autoSuccess color="green" >
-              Fetching Categories
-            </Progress>
-
-          </div>
-
-        );
-    } else {
-      return (
-        <div>
-          {toastr.clear()}
-          <p>Am here</p>
-        </div>
-      )
-    }
-
+        <Segment color="teal" loading={this.state.fetchingCategories}>
+          <Header textAlign="center" size="medium">EXISTING CATEGORIES</Header>
+          <CategoryList categories={this.state.categories} />
+        </Segment>
+      </Segment.Group>
+    );
 
   }
-}
-
-CategoryPage.propTypes = {
-  actions: PropTypes.object.isRequired,
-  categories: PropTypes.array.isRequired
-};
-
-function mapStateToProps(state, ownProps) {
-  return {
-    categories: state.categoryReducer   // We will access it by this.props.courses in our Component
-  };
 }
 
 export default CategoryPage;
 
+
 /*
+<Segment>
+  <Progress percent={this.state.percent} color="green" size="tiny" autoSuccess >
+    Fetching Categories
+  </Progress>
+</Segment>
+
+<Segment>
+  <Progress percent={this.state.percent} size="tiny"
+    autoSuccess color="green" >
+    Categories fetched Successfully
+  </Progress>
+</Segment>
+
 <Progress percent={this.state.percent}
   autoSuccess color="green" >
   Fetching Categories
