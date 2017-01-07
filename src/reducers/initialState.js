885836d08905ac3602223f38d28066fb400436c7
barneyJomo <brnbsjm@gmail.com>
2017-01-07 13:58:58 +0300

    CHANGE OF FOLDER STRUCTURE AND APP ENTRY

--- src/reducers/initialState.js
+++
@@ -1,8 +0,0 @@
-export default {
-  userSessionCredentials: !!window.localStorage.shopID_token
-  //userSessionCredentials: window && window.localStorage ? window.localStorage.getItem('shopID_token') : null
-};
-
-
-// window.localStorage.getItem('id_token') will return null if key not found
-// token: window && window.localStorage ? window.localStorage.getItem('id_token') : null,
